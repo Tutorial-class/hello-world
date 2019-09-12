fc8a96c4fcd1f851422eecc5f0c57b3514f650c1
Tutorial-class <54949182+Tutorial-class@users.noreply.github.com>
2019-09-12 08:50:34 -0600

    Update tutorial.js

--- tutorial.js
+++ tutorial.js
@@ -1,9 +1,24 @@
-print("Hello World")
+geetMe("Hello World")//When you open a web page, this will apear
 
 <html>
   <head>
-  <title>blackbox example </title>
+  <title>
   </script>
 </head>
 <body>
     <button>
+    <font>
+  <text>
+  <footer>
+<html>
+
+//This is the format for an expample web page
+  
+  //Now let's to write variables!
+  
+  
+  let (blank) = function()//Anyone just say a random word.
+  
+  if (blank) then //This is really simple, but you get the idea that now the word we just said is defined.
+  
+  
